b9fc17001ba4db82b3b5d8438ada426001747dcc
olga_mirocz <ol.miroch@gmail.com>
2022-03-22 13:32:14 +0100

    initial commit

---
+++ stock-trader/src/components/portfolio/Portfolio.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Porfolio page</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ stock-trader/src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/Home.vue'
+import Stocks from './components/stocks/Stocks.vue'
+import Portfolio from './components/portfolio/Portfolio.vue'
+
+export const routes = [
+  { path: '/', component: Home },
+  { path: '/stocks', component: Stocks },
+  { path: '/portfolio', component: Portfolio }
+]
