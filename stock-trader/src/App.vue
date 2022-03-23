d97a9c3d15de1ec89e1651b8bdb9626d86d79a59
olga_mirocz <ol.miroch@gmail.com>
2022-03-23 14:03:37 +0100

    add animation on route changing

--- stock-trader/src/App.vue
+++ stock-trader/src/App.vue
@@ -3,7 +3,9 @@
       <app-header />
       <div class="row">
         <div class="col-md-12">
-          <router-view />
+          <transition name="slide" mode="out-in">
+            <router-view />
+          </transition>
         </div>
       </div>
     </div>
@@ -23,7 +25,37 @@ export default {
 </script>
 
 <style>
-body {
-  padding: 30px;
-}
+  body {
+    padding: 30px;
+  }
+
+  .slide-enter-active {
+    animation: slide-in .2s ease-out forwards;
+  }
+
+  .slide-leave-active {
+    animation: slide-out .2s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(-30px);
+      opacity: 0;
+    }
+    to {
+      ransform: translateX(0);
+      opacity: 1;
+    }
+  }
+
+  @keyframes slide-out {
+    from {
+      transform: translateX(0);
+      opacity: 1;
+    }
+    to {
+      ransform: translateX(-30px);
+      opacity: 0;
+    }
+  }
 </style>
